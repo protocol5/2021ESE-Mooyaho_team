31c8b7b7fc42e6ed4276dce0ae99d60469bd3542
han45699 <han45699@naver.com>
2021-05-12 19:16:55 +0900

    New python file : real-time image pop-up code for Raspberry Pi server

---
+++ image_popup.py
@@ -0,0 +1,45 @@
+import cv2
+import os
+import time
+
+# image file path
+img_file = '/home/myh/2021ESE-Mooyaho_team/darknet/fall_detected.jpg'
+
+# get modified time of image first
+modTimesinceEpoc = os.path.getmtime(img_file)
+modificationTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(modTimesinceEpoc))
+print("Last Modified Time : ", modificationTime)
+
+count = 0
+# keep loop for showing image (when modified time is changed)
+while(1):
+    # get modified time again to compare
+    modTimesinceEpoc_recent = os.path.getmtime(img_file)
+    modificationTime_recent = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(modTimesinceEpoc_recent))
+    print("Last Modified Time : ", modificationTime_recent)
+
+    # if modified time is different with before time, count = 1
+    if modificationTime_recent != modificationTime:
+        count = 1
+    # else, count = 0
+    else:
+        count = 0
+
+    # if count = 1, wait 3 seconds for rewriting the image and imshow for 2 seconds
+    if count == 1:
+        time.sleep(3)
+        image = cv2.imread(img_file, cv2.IMREAD_COLOR)
+        cv2.imshow('wow', image)
+        cv2.waitKey(2000)
+    # else, close the image and continue
+    else:
+        cv2.destroyAllWindows()
+        continue
+
+    # renewal the variable (modified time)
+    modificationTime = modificationTime_recent
+
+cv2.destroyAllWindows()
+
+
+
